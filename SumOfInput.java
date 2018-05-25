bbf04bf9d268f906bc26c41a89e0413ef9e4b67b
Tommy Rogers <tommyr@TommyRogers.local>
2018-05-25 07:50:27 -0400

    hope this works

---
+++ SumOfInput.java
@@ -0,0 +1,20 @@
+/**
+ * Created by iyasuwatts on 10/17/17.
+ */
+import java.util.*;
+public class SumOfInput {
+
+    public static void main(String[] args){
+    Scanner scan = new Scanner(System.in);
+    for(int x = 0; x < 100; x++){
+    System.out.println("Enter a number");    
+    int input = scan.nextInt();
+    int sum = input + 1;
+    System.out.println(sum);
+    }
+    
+    
+    
+    
+    }
+}
